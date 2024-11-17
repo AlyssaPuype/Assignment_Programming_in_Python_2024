c55add34d6680436dc74041c85e768ca12766087
Alyayeex <alyssa.puype@gmail.com>
2024-11-17 17:45:23 +0100

    added methods in application file to interact w the database

--- application_manager.py
+++ application_manager.py
@@ -1,1 +1,23 @@
 # interaction with database to add, remove, edit, fetch data from the database
+
+class StudyTracker:
+
+	def __init__(self, db: Database):
+		self.db = db
+
+
+	def addCourse():
+
+	def removeCourse():
+
+	def getCourse():
+
+	def updateCourse():
+
+	def addSession():
+
+	def removeSession():
+
+	def getSession():
+
+	def updateSession():
